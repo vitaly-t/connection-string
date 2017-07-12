94fe95450d2095eeb74579e06fdbf2f14a07c1a0
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2017-07-12 03:08:17 +0100

    adding tests.

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -2,6 +2,26 @@
 
 var parse = require('../lib');
 
+describe('main', function () {
+    it('must throw on a non-string', function () {
+        var error = new TypeError('Invalid connection string.');
+        expect(function () {
+            parse();
+        }).toThrow(error);
+        expect(function () {
+            parse(123);
+        }).toThrow(error);
+    });
+    it('must allow an empty string', function () {
+        // TODO: Needs fixing:
+        /*
+        expect(parse('')).toEqual({
+            segments: [],
+            params: {}
+        });*/
+    });
+});
+
 describe('protocol', function () {
     it('must recognize standard format', function () {
         expect(parse('abc://')).toEqual(jasmine.objectContaining({
@@ -21,4 +41,28 @@ describe('protocol', function () {
             protocol: 'abc'
         }));
     });
+});
+
+describe('user', function () {
+
+});
+
+describe('password', function () {
+
+});
+
+describe('host', function () {
+
+});
+
+describe('port', function () {
+
+});
+
+describe('segments', function () {
+
+});
+
+describe('params', function () {
+
 });
