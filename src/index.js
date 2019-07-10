f79ee1513fb6bfcc5ec8317fe7fc28947c432097
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-07-10 22:28:42 +0100

    refactoring host regex

--- src/index.js
+++ src/index.js
@@ -121,14 +121,14 @@
         var m, isIPv6;
         if (host[0] === '[') {
             // This is IPv6, with [::] being the shortest possible
-            m = host.match(/((\[[0-9a-z:%]{2,45}])(?::(-?[0-9a-z]+[^/?]*))?)/i);
+            m = host.match(/((\[[0-9a-z:%]{2,45}])(?::(-?[0-9a-z]+))?)/i);
             isIPv6 = true;
         } else {
             // It is either IPv4 or domain/socket
             if (raw) {
-                m = host.match(/(([a-z0-9.$/-]*)(?::(-?[0-9a-z]+[^/?]*))?)/i);
+                m = host.match(/(([a-z0-9.$/-]*)(?::(-?[0-9a-z]+))?)/i);
             } else {
-                m = host.match(/(([a-z0-9.$%-]*)(?::(-?[0-9a-z]+[^/?]*))?)/i);
+                m = host.match(/(([a-z0-9.$%-]*)(?::(-?[0-9a-z]+))?)/i);
             }
         }
         if (m) {
