a244cd26c9ac069d27f44f24fe8e542051aeff5c
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-08-25 10:07:08 +0100

    updating jest config

--- jest.config.js
+++ jest.config.js
@@ -3,25 +3,26 @@ module.exports = {
     collectCoverage: true,
     coverageThreshold: {
         global: {
-            branches: 95, // Interoperability JS lines cannot be covered, hence 95%
-            lines: 100
+            branches: 95,
+            lines: 100,
         }
     },
-    globals: {
-        'ts-jest': {
-            tsConfig: {
-                target: 'ES2020',
+    roots: [
+        './test',
+    ],
+    testMatch: [
+        '**/?(*.)+(spec|test).+(ts|tsx|js)',
+        '**/__tests__/**/*.+(ts|tsx|js)'
+    ],
+    transform: {
+        '^.+\\.(ts|tsx)$': [
+            'ts-jest',
+            {
+                tsconfig: {
+                    target: 'ES2020',
+                    esModuleInterop: true
+                }
             }
-        }
-    },
-    "roots": [
-        "./test"
-    ],
-    "testMatch": [
-        "**/__tests__/**/*.+(ts|tsx|js)",
-        "**/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
-    "transform": {
-        "^.+\\.(ts|tsx)$": "ts-jest"
+        ]
     }
 }
